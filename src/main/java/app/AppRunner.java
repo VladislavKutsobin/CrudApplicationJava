f0f9f9fe258cef854052e381c24f82dba71c357d
Vladislav Kutsobin <kutsobin_vladislav@rambler.ru>
2018-09-08 23:31:55 +0300

    Adding update(developers dev) and testing

--- src/main/java/app/AppRunner.java
+++ src/main/java/app/AppRunner.java
@@ -3,9 +3,11 @@ package main.java.app;
 import main.java.app.model.Account;
 import main.java.app.model.Developers;
 import main.java.app.model.Skill;
+import main.java.app.repository.AccountRepository;
 import main.java.app.repository.DeveloperRepository;
 import main.java.app.repository.SkillRepository;
 import main.java.app.repository.io.JavaDeveloperRepositoryImpl;
+import main.java.app.repository.io.JavaIOAccountRepositoryImpl;
 import main.java.app.repository.io.JavaIOSkillRepositoryImpl;
 
 import java.io.IOException;
@@ -15,26 +17,48 @@ import java.util.Set;
 
 public class AppRunner {
     public static void main(String[] args) throws IOException {
-        DeveloperRepository dr = new JavaDeveloperRepositoryImpl();
+        SkillRepository sr = new JavaIOSkillRepositoryImpl();
+        AccountRepository ar = new JavaIOAccountRepositoryImpl();
+        DeveloperRepository dv = new JavaDeveloperRepositoryImpl();
 
-        Account acc = new Account(2L, "Vladimir");
-        Account acc1 = new Account(3L, "Vladislav");
-        Skill skill1 = new Skill(1L, "SQL");
-        Skill skill2 = new Skill(2L, "Java");
+        Account ac1 = new Account(1L,"Maya");
+        Account ac2 = new Account(2L,"Bob");
 
-        Set<Long> skillSet1 = new HashSet<>();
+
+        Skill skill1 = new Skill(1L,"SQL");
+        Skill skill2 = new Skill(2L,"C");
+        Skill skill3 = new Skill(3L,"Java");
+
+        Skill skill4 = new Skill(4L,"Python");
+        Skill skill5 = new Skill(5L,"Django");
+
+        Skill skill6 = new Skill(6L,"JavaScript");
+        Skill skill7 = new Skill(7L,"HTML");
+
+        Set<Long>  skillSet1 = new HashSet<>();
         skillSet1.add(skill1.getId());
         skillSet1.add(skill2.getId());
-        Set<Long> skillSe12 = new HashSet<>();
-        skillSe12.add(skill1.getId());
 
+        Set<Long > skillSet2 = new HashSet<>();
+        skillSet2.add(skill3.getId());
+        skillSet2.add(skill4.getId());
+        skillSet2.add(skill5.getId());
 
-        Developers dev1 = new Developers(skillSet1, acc);
-        dr.save(dev1);
-        Developers dev2 = new Developers(skillSe12, acc1);
-        dr.save(dev2);
+        Developers dev1 = new Developers(skillSet1,ac1);
+        Developers dev2 = new Developers(skillSet2,ac2);
 
-        System.out.println(dr.findAll());
+        dv.save(dev1);
+        dv.save(dev2);
 
+        Set<Long> skillSet3 = new HashSet<>();
+        skillSet3.add(skill6.getId());
+        skillSet3.add(skill7.getId());
+
+        Account acTest = new Account(1L,"TestSkill"); //id = 1 ==> is the same as in ac1
+        Developers devTest = new Developers(skillSet3,acTest);
+        dv.update(devTest);
+
+        System.out.println(dv.findAll());
+        System.out.println(dv.getById(2L));
     }
 }
--- src/main/java/app/repository/io/JavaDeveloperRepositoryImpl.java
+++ src/main/java/app/repository/io/JavaDeveloperRepositoryImpl.java
@@ -56,16 +56,93 @@ public class JavaDeveloperRepositoryImpl implements DeveloperRepository {
 
     @Override
     public void update(Developers developers) throws IOException {
+        // TODO update Set<LongID> skills, Account account
+        File fileWithDev = new File(DEVELOPER_FILE_PATH);
+        List<String> list = new ArrayList<>();
 
+        Account account= developers.getAccount();
+        Set<Long> setSkills = developers.getSkills();
+        String strIdForComparing = Long.toString(account.getId());
+        String line;
+
+        FileReader fr = new FileReader(fileWithDev);
+        try (BufferedReader br = new BufferedReader(fr)) {
+            while ((line = br.readLine()) != null) {
+                list.add(line);
+            }
+
+            for (int i = 0; i < list.size(); i++) {
+                String[] arrOfStr = list.get(i).split("/");
+                if (strIdForComparing.equals(arrOfStr[0])) {
+                    list.set(i,account + "." + setSkills);
+                    break;
+                }
+            }
+
+            FileWriter fw = new FileWriter(fileWithDev, false);
+            try (BufferedWriter bw = new BufferedWriter(fw)) {
+                for (String str : list) {
+                    bw.write(str + "\n");
+                }
+            }
+        }
     }
 
     @Override
     public void delete(Long aLong) throws IOException {
+        File fileWithDevelopers = new File(DEVELOPER_FILE_PATH);
+        List<String> list = new ArrayList<>();
+        FileReader fr = new FileReader(fileWithDevelopers);
+        String line;
+
+        try (BufferedReader bf = new BufferedReader(fr)) {
+            while ((line = bf.readLine()) != null) {
+
+                String[] arrOfStr = line.split("/");
+                if(arrOfStr[0].equals(Long.toString(aLong))){
+                    continue;
+                }else { list.add(line);
+                }
+            }
+            // write updated content without deleted item from ArrayList to file:
+            FileWriter fw = new FileWriter(fileWithDevelopers, false);
+            try (BufferedWriter bw = new BufferedWriter(fw)) {
+                for (String str : list) {
+                    bw.write(str + "\n");
+                }
+            }
+        }
 
     }
 
     @Override
     public Developers getById(Long aLong) throws FileNotFoundException, IOException {
+        File file = new File(DEVELOPER_FILE_PATH);
+        FileReader fr = new FileReader(file);
+        String line;
+
+        try (BufferedReader br = new BufferedReader(fr)) {
+            while ((line = br.readLine()) != null) {
+                String[] arrOfStr = line.split("/");
+                System.out.println(arrOfStr[0].equals((Long.toString(aLong))));
+                if(arrOfStr[0].equals(Long.toString(aLong))){
+                    String [] arrOfStr2 =  arrOfStr[1].split("\\.");
+                    String delBracket = arrOfStr2[1].substring(1,arrOfStr2[1].length()-1);
+                    String [] arrOfStr3 = delBracket.split(", ");
+                    Set<String> setOfStrId = new HashSet<>(Arrays.asList(arrOfStr3));
+
+                // Converting Set<String> to Set<Long>
+                    Set<Long> setOfId = new HashSet<>();
+                    for(String s : setOfStrId){
+                        Long g = Long.parseLong(s);
+                        setOfId.add(g);
+                    }
+
+                    return new Developers(setOfId, new Account(Long.parseLong(arrOfStr[0]),arrOfStr2[0]));
+                }
+            }
+        }
+
         return null;
     }
 }
